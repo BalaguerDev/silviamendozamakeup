5f78fbef715216e9dd902415c7c9761265fd8453
BalaguerDev <balaguer.dev@gmail.com>
2024-11-28 21:27:58 +0100

    feat(Navbar): :sparkles: creacion de navbar, falta definir estilos y funcionalidad

---
+++ src/constants/navLinks.js
@@ -0,0 +1,9 @@
+export const navLinks = [
+    { name: "Novias", href: "/novias" },
+    { name: "Servicios", href: "/servicios" },
+    { name: "Cursos", href: "/cursos" },
+    { name: "Galería", href: "/galeria" },
+    { name: "Sobre Mí", href: "/sobremi" },
+    { name: "Contacto", href: "/contacto" },
+    { name: "Blog", href: "/blog" }
+  ];
