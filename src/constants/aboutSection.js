7ceab4c97893c492e6197637eab420320fd95edd
BalaguerDev <balaguer.dev@gmail.com>
2024-12-02 18:35:02 +0100

    feat: :sparkles: seccion AboutMe del homepage

---
+++ src/constants/aboutSection.js
@@ -0,0 +1,41 @@
+export const reasonsToChoose = [
+    {
+        title: "Maquillaje nupcial personalizado:",
+        description:
+            "Creamos looks atemporales que realzan tu belleza natural y se adaptan perfectamente a tu estilo personal.",
+    },
+    {
+        title: "Peinados de ensueño:",
+        description:
+            "Desde recogidos elegantes hasta melenas sueltas con ondas, encontraremos el peinado perfecto para tu rostro y vestido.",
+    },
+    {
+        title: "Asesoramiento personalizado:",
+        description:
+            "Te guiaré para encontrar el look que siempre has soñado hasta el día de Sí, quiero!",
+    },
+    {
+        title: "Maquillaje a prueba de lágrimas:",
+        description: "Duradero para que brilles en la pista de baile.",
+    },
+];
+
+
+
+export const additionalServices = [
+    {
+        title: "Cursos de automaquillaje y autopeinado:",
+        description:
+            "Aprende las técnicas profesionales para maquillarte y peinarte como una experta en cualquier ocasión.",
+    },
+    {
+        title: "Asesoría de imagen integral:",
+        description:
+            "Descubre tu estilo personal y potencia tu imagen en todas las ocasiones.",
+    },
+    {
+        title: "Cursos de formación profesional:",
+        description:
+            "¿Quieres convertir tu pasión por la belleza en tu profesión? Ofrecemos cursos especializados para maquilladores y estilistas.",
+    },
+];
