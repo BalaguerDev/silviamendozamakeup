bbf153b743df8e442f25b3472bcae3bbb1a310d6
BalaguerDev <balaguer.dev@gmail.com>
2024-12-20 15:23:43 +0100

    feat: :sparkles: metadas

--- src/constants/seo.js
+++ src/constants/seo.js
@@ -1,5 +1,5 @@
 export const SEO = {
-    DEFAULT_TITLE: "Maquillaje y peinado de novia en Barcelona",
+    DEFAULT_TITLE: "Maquillaje y Peluquería para Novias en Barcelona | Silvia Mendoza Make Up",
     DEFAULT_DESCRIPTION: "¿Buscas una maquilladora para tu boda en Barcelona? Silvia Mendoza Makeup ofrece servicios de maquillaje y peinado para novias e invitadas.",
     DEFAULT_KEYWORDS: ["maquillaje para novia en Barcelona", "peinado para novia en barcelona", "maquilladora en barcelona", "asesora de imagen en barcelona", "automaquillaje", "curso de automaquillaje", "maquillaje novias"],
     DEFAULT_IMAGE:"/images/homepage/aboutme/Maquilladora-profesional-para-novias-en-barcelona.webp",
