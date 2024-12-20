095060ce0fcae14861bacfb92a374787c64587ef
BalaguerDev <balaguer.dev@gmail.com>
2024-12-20 16:24:58 +0100

    feat: :sparkles: refactor

---
+++ src/data/index.js
@@ -0,0 +1,7 @@
+export * from "./aboutSection";
+export * from "./FAQ";
+export * from "./courseList";
+export * from "./galleriesList";
+export * from "./packNovia";
+export * from "./reviewsGoogle";
+export * from "./serviceList";
