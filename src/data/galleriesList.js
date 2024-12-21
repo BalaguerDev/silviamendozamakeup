c938cd55bc94569447fd06997d12bf3c90439939
BalaguerDev <balaguer.dev@gmail.com>
2024-12-21 22:54:31 +0100

    refactor: :sparkles: refactor

--- src/data/galleriesList.js
+++ src/data/galleriesList.js
@@ -1,14 +1,16 @@
-export const generateImagePaths = (folder, numberOfImages, altTextPrefix) => {
-    return Array.from({ length: numberOfImages }, (_, i) => {
-      return {
-        src: `/images/galleries/${folder}/image-${i + 1}.webp`,
-        alt: `${altTextPrefix} ${i + 1}`
-      };
-    });
-  };
-  
-  export const galleriesList = {
-    shootings: generateImagePaths('shooting', 12, 'Sesión de Shooting + Maquillaje y peluqueria en Barcelona'),
-    novias: generateImagePaths('novias', 12, 'Maquillaje y peinado para novia en Barcelona'),
-    inspiracion: []
-  };
+export const generateImagePaths = (basePath, folder, numberOfImages, altTextPrefix) => {
+  const folderPath = folder ? `/${folder}` : '';
+  return Array.from({ length: numberOfImages }, (_, i) => {
+    return {
+      src: `${basePath}${folderPath}/image-${i + 1}.webp`,
+      alt: `${altTextPrefix} ${i + 1}`
+    };
+  });
+};
+
+export const galleriesList = {
+  shootings: generateImagePaths('/images/galleries', 'shooting', 12, 'Sesión de Shooting + Maquillaje y peluqueria en Barcelona'),
+  novias: generateImagePaths('/images/galleries', 'novias', 12, 'Maquillaje y peinado para novia en Barcelona'),
+  inspiracion: [],
+  colaboradores: generateImagePaths('/images/sobremi/colaboradores', '', 4, 'Colaboradores destacados de Sílvia Mendoza')
+};
